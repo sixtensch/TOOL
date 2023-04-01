3b9d4745f8fadf4b918004880d9e07a36e7c9aa8
crazysissen <crazysissen@gmail.com>
2023-04-01 04:38:11 +0200

    build: Added config log to build, and updated some related settings.

--- build.bat
+++ build.bat
@@ -3,5 +3,7 @@
 if not defined VCToolsVersion call vcvarsall x64
 if [%1] == [] (set ConfigName=Debug) else set ConfigName=%1
 
+echo Building %ConfigName%
+
 msbuild TOOL.vcxproj /p:configuration=%ConfigName% /v:m
 msbuild Test.vcxproj /p:configuration=%ConfigName% /v:m
