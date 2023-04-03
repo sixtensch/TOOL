5d857615c7547051354b874edf656ea3e94a986c
crazysissen <crazysissen@gmail.com>
2023-04-03 11:19:32 +0200

    feat: Basic copy functions (and minor vcxproj changes).

---
+++ Source/Basics.cpp
@@ -0,0 +1,43 @@
+
+#include "Basics.h"
+
+#include "Windows.h"
+
+
+
+// TODO(crazy): Extend these to work on Linux and MacOS
+
+namespace Tool
+{
+    void Copy(void* destination, const void* source, u64 size)
+    {
+        CopyMemory(destination, source, size);
+    }
+    
+    void Copy(void* destination, const void* source, u64 count, u64 size)
+    {
+        Copy(destination, source, count * size);
+    }
+    
+    u64 CStringLength(const c8* string, u64 capacity)
+    {
+        u64 size = 0;
+        for (u64 i = 0; i < capacity - 1; i++)
+        {
+            size = i;
+            if (string[i] == '\0')
+            {
+                break;
+            }
+        }
+        
+        return size;
+    }
+    
+    u64 CStringCopy(c8* destination, const c8* source, u64 capacity)
+    {
+        u64 size = CStringLength(source);
+        Copy((void*)destination, (const void*)source, size);
+        return size;
+    }
+}
--- Source/Basics.h
+++ Source/Basics.h
@@ -3,6 +3,10 @@
 
 
 
+//- Acronyms
+
+//~ Acronym definitions
+
 namespace Tool
 {
     typedef signed char        i8;
@@ -17,12 +21,17 @@ namespace Tool
     
     typedef float              f32;
     typedef double             f64;
+    typedef long double        f128;
     
     typedef bool               b8;
     typedef int                b32;
+    
+    typedef char               c8;
+    typedef unsigned short     c16;
+    typedef unsigned int       c32;
 }
 
-
+//~ Acronym usings
 
 #ifndef TOOL_NO_ACRONYMS
 
@@ -38,12 +47,29 @@ using Tool::u64;
 
 using Tool::f32;
 using Tool::f64;
+using Tool::f128;
 
 using Tool::b8;
 using Tool::b32;
 
+using Tool::c8;
+using Tool::c16;
+using Tool::c32;
+
 #endif //TOOL_NO_ACRONYMS
 
+
+
+//- Most basic functions
+
+namespace Tool
+{
+    void Copy(void* destination, const void* source, u64 size);
+    void Copy(void* destination, const void* source, u64 count, u64 size);
+    
+    u64 CStringLength(const c8* string, u64 capacity = 0x10000); 
+    u64 CStringCopy(c8* destination, const c8* source, u64 capacity); // Copies up to capacity - 1 characters. 
+}
 
 
 #endif
