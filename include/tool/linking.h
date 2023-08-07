6796163c9cfb51cf8d7578743822ad0baebae680
crazysissen <crazysissen@gmail.com>
2023-08-07 23:24:22 +0200

    Removed module cast functions.

--- include/tool/linking.h
+++ include/tool/linking.h
@@ -31,14 +31,6 @@ namespace Tool
     // These can represent either function pointers or variables.
     void* ModuleGetSymbol(Module module, const c8* name);
     
-    // Retrieve a variable pointer from a loaded module.
-    template<typename T>
-        inline T* ModuleGetVariable(Module module, const c8* name) { return (T*)ModuleGetSymbol(module, name); }
-    
-    // Retrieve a functin pointer from a loaded module.
-    //template<typename T_return, typename... T_params>
-    //inline T_return (*ModuleGetFunction(Module module, const c8* name))(int, int) { return (T_return (*)(T_params...))ModuleGetSymbol(module, name); }
-    
 }
 
 #endif //_LINKING_H
